fcd472c164c37fdeb1c3c934eb95d3dab43f043b
justin_maai1 <justin9300454@gmail.com>
2022-11-17 12:55:20 +0000

    Fix 'infile' and 'outfile' path

--- pains_procedures.py
+++ pains_procedures.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     subparsers.required = True
 
     parser_f1 = subparsers.add_parser("fliter_pains", help="Apply PAINS filter to query compounds")
-    parser_f1.add_argument("-q_mol", "--query_file", help="The path of the input file that contains query compounds to be performed with PAINS filter", type=argparse.FileType('r'), dest= "infile", required=True)
-    parser_f1.add_argument("-out", "--output_file", help="The path of the output file that contains result of Non-PAINS compounds", type=argparse.FileType('w'), dest= "outfile", required=True)
+    parser_f1.add_argument("-q_mol", "--query_file", help="The path of the input file that contains query compounds to be performed with PAINS filter", dest= "infile", required=True)
+    parser_f1.add_argument("-out", "--output_file", help="The path of the output file that contains result of Non-PAINS compounds", dest= "outfile", required=True)
     
     args = parser.parse_args()
     pains = PainsFilter()
