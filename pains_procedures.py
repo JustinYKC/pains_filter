d608d4be584e76fb3fa87434ba175843bbe70d86
justin_maai1 <justin9300454@gmail.com>
2022-11-17 01:06:16 +0000

    Add subparsers: 'input file' and 'output file'

--- pains_procedures.py
+++ pains_procedures.py
@@ -1,4 +1,5 @@
 import argparse
+from painsFilter import PainsFilter
 
 
 if __name__ == "__main__":
@@ -6,6 +7,12 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers(dest= 'subcmd', help='subcommands', metavar='SUBCOMMAND')
     subparsers.required = True
 
+    parser_f1 = subparsers.add_parser("fliter_pains", help="Apply PAINS filter to query compounds")
+    parser_f1.add_argument("-q_mol", "--query_file", help="The path of the input file that contains query compounds to be performed with PAINS filter", type=argparse.FileType('r'), dest= "query file", required=True)
+    parser_f1.add_argument("-out", "--output_file", help="The path of the output file that contains result of Non-PAINS compounds", type=argparse.FileType('w'), dest= "output file", required=True)
     
-
-
+    args = parser.parse_args()
+    pains = PainsFilter()
+    
+    if args.subcmd == "fliter_pains":
+        pains.filter_by_pains(input_smiles_path, output_smiles_path)
